b543911ec747d2007c87adc712d59d94545c7064
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2021-12-19 13:10:38 +0100

    module structure finished, every requirements will be stored

--- app/index.js
+++ app/index.js
@@ -8,16 +8,59 @@ let hideout_modules = {};
         module_name,
         levels: [
             0 => [
-                0 => {
-                    item_name: item_name,
-                    item_amount: int,
-                    item_link: link
-                },
-                1 => {
-                    item_name: item_name1,
-                    item_amount: int,
-                    item_link: link
-                },
+                items: [
+                    0 => {
+                        item_name: item_name,
+                        item_amount: int,
+                        item_link: link
+                    },
+                    1 => {
+                        item_name: item_name1,
+                        item_amount: int,
+                        item_link: link
+                    },
+                ], 
+                money: [
+                    'string', 'string;
+                ],
+                trader_lvl: [
+                    0 => {
+                        trader_name: trader_name,
+                        trader_level: req_level,
+                        trader_link: link
+                    },
+                    1 => {
+                        trader_name: trader_name1,
+                        trader_level: req_level,
+                        trader_link: link
+                    },
+                ],
+                hideout_module: [
+                    0 => {
+                        module_name: module,
+                        module_level: req_level,
+                        module_link: link
+                    },
+                    1 => {
+                        module_name: module1,
+                        module_level: req_level,
+                        module_link: link
+                    },
+                ],
+                player_stat: [
+                    0 => {
+                        stat_name: stat,
+                        stat_level: req_level,
+                        stat_link: link
+                    },
+                    1 => {
+                        stat_name: stat1,
+                        stat_level: req_level,
+                        stat_link: link
+                    },
+                ]
+
+                
             ]
         ]
     }
