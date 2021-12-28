117f0cc3fa4ed88c2eec8cd43455cbc8afd49720
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2021-12-28 11:47:02 +0100

    removed unnecessary lines

--- app/index.js
+++ app/index.js
@@ -1,73 +1,9 @@
 const hideoutScraper = require("./scrapers/hideoutScraper");
+const questScraper = require("./scrapers/questScraper");
 
-const WIKI_URL = "https://escapefromtarkov.fandom.com/wiki/";
-let hideout_modules = {};
+//hideoutScraper.getHideoutModules();
+questScraper.getQuestItems();
 
-/*hideout_modules = [
-    0 => {
-        module_name,
-        levels: [
-            0 => {
-                items: [
-                    0 => {
-                        item_name: item_name,
-                        item_amount: int,
-                        item_link: link
-                    },
-                    1 => {
-                        item_name: item_name1,
-                        item_amount: int,
-                        item_link: link
-                    },
-                ], 
-                money: [
-                    'string', 'string;
-                ],
-                trader_lvl: [
-                    0 => {
-                        trader_name: trader_name,
-                        trader_level: req_level,
-                        trader_link: link
-                    },
-                    1 => {
-                        trader_name: trader_name1,
-                        trader_level: req_level,
-                        trader_link: link
-                    },
-                ],
-                hideout_module: [
-                    0 => {
-                        module_name: module,
-                        module_level: req_level,
-                        module_link: link
-                    },
-                    1 => {
-                        module_name: module1,
-                        module_level: req_level,
-                        module_link: link
-                    },
-                ],
-                player_stat: [
-                    0 => {
-                        stat_name: stat,
-                        stat_level: req_level,
-                        stat_link: link
-                    },
-                    1 => {
-                        stat_name: stat1,
-                        stat_level: req_level,
-                        stat_link: link
-                    },
-                ],
-                purchase: [
-                    string, string
-                ]
-            }
-        ]
-    }
-]*/
-
-hideoutScraper.getHideoutModules(WIKI_URL);
 
 
 
